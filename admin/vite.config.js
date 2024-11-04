da9376a2760a30461a304c6ca3a1a922a7a69ee1
Abhijeet Dutta <93587547+abhijeetdutta3008@users.noreply.github.com>
2024-11-04 19:47:19 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:5174}
-     host: true,  // This enables the app to be accessible on your local network, useful for testing.
-  },
-  build: {
-    outDir: 'dist',  // Default is 'dist', change if needed.
-  }
+
 })
